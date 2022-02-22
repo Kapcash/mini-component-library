809b444e7c7b890ad81228773eea3b99f27105d5
Florent Catiau-Tristant <florent.ct@orange.fr>
2022-02-22 15:01:16 +0100

    feat(Select): implement global style, invisible select tag

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,38 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      {displayedValue}
+      <Icon id="chevron-down" size="16" />
+      <StyledSelect value={value} onChange={onChange}>
+        {children}
+      </StyledSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  padding: 12px 16px;
+  border-radius: 8px;
+  display: inline-flex;
+
+  position: relative;
+
+  background: ${COLORS.transparentGray15};
+  color: ${COLORS.gray500};
+`
+
+const StyledSelect = styled.select`
+  appearance: none;
+  border: none;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  visibility: hidden;
+`
 
 export default Select;
